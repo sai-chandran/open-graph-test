647082204dcbc89956835e32533759b8c6f5805a
Sai Chandran <sai.chandran@icompaas.in>
2024-08-21 15:51:42 +0000

    Updating banner image

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content="Cloud Infrastructure Compliance and Enhanced CyberSecurity | iCompaas" />
         <meta property="og:description" content="iCompaas is a Cloud Infrastructure CyberSecurity, Compliance and Cost Optimization Tool. Cloud Security Posture Management Tool." />
-        <meta property="og:image" content="/banner.jpg" />
+        <meta property="og:image" content="/logo.jpg" />
         <meta property="og:url" content="https://www.icompaas.com" />
       </head>
       <body>{children}</body>
